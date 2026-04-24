58c14985ca500cfd64156053eb3f8fe9f2c40b43
Flurop <flora@ratgers.nl>
2026-04-24 21:23:59 +0200

    New using real api

--- signup.js
+++ signup.js
@@ -19,7 +19,7 @@ form.addEventListener('submit', async (event) => {
     }
 
     try {
-        const response = await fetch('http://localhost:3333/api/signup', {
+        const response = await fetch('http://orbo.api.orbinuity.nl/api/signup', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
