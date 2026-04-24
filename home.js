15b05cd56f9ac2d6370c90cfd43ae70780f99c2c
Flurop <flora@ratgers.nl>
2026-04-24 10:02:39 +0200

    Home page code

---
+++ home.js
@@ -0,0 +1,38 @@
+const token = document.cookie
+    .split("; ")
+    .find(row => row.startsWith('token='))
+    ?.split("=")[1];
+
+if (!token) document.location.href = "/";
+
+async function init(title) {
+    const response = await fetch('http://localhost:3333/api/userinfo', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    
+    const data = await response.json();
+    
+    if (response.ok) {
+        title.textContent += ", "+data.user.name;
+    } else {
+        title.textContent += " to Orbo"
+    }
+}
+
+const observera = new MutationObserver((mutations, obs) => {
+    const title = document.getElementById("title");
+
+    if (title) {
+        obs.disconnect()
+        init(title)
+    }
+});
+
+observera.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+});
