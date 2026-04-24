0f3fda3029ed78f9d97688ee6eedc2a728347000
Flurop <flora@ratgers.nl>
2026-04-24 10:03:18 +0200

    Welcome, signin & signup page code

---
+++ index.js
@@ -0,0 +1,34 @@
+async function testToken(token, headerNav) {
+    if (token) {
+        const response = await fetch('http://localhost:3333/api/userinfo', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if (response.ok) document.location.href = "/home";
+        else headerNav.innerHTML = "";
+    } else headerNav.innerHTML = "";
+}
+
+const observerb = new MutationObserver((mutations, obs) => {
+    const headerNav = document.getElementById("header-nav");
+    
+    if (headerNav) {
+        obs.disconnect(); 
+
+        const token = document.cookie
+            .split("; ")
+            .find(row => row.startsWith('token='))
+            ?.split("=")[1];
+
+        testToken(token, headerNav)
+    }
+});
+
+observerb.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+});
