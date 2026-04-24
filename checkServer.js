8830595d1349ae18d16ea9f1e290ef3e379110fe
Flurop <flora@ratgers.nl>
2026-04-24 21:23:54 +0200

    New using real api

--- checkServer.js
+++ checkServer.js
@@ -1,6 +1,6 @@
 async function init() {
     try {
-        const response = await fetch('http://localhost:3333/api/isonline', {
+        const response = await fetch('http://orbo.api.orbinuity.nl/api/isonline', {
             method: 'GET'
         });
 
