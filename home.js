f5d8bf2cb5e51bb8323e893289969c0f7f50ebb8
Flurop <flora@ratgers.nl>
2026-04-25 09:04:46 +0200

    Now using correct domain!

--- home.js
+++ home.js
@@ -6,7 +6,7 @@ const token = document.cookie
 if (!token) document.location.href = "/";
 
 async function init(title) {
-    const response = await fetch('http://89.184.185.202:55555/api/userinfo', {
+    const response = await fetch('https://orbo-api.orbinuity.nl/api/userinfo', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
