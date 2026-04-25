f808dfa256bf5fd3d5e4312f5331e49677542789
Flurop <flora@ratgers.nl>
2026-04-25 09:04:47 +0200

    Now using correct domain!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function testToken(token, headerNav) {
     if (token) {
-        const response = await fetch('http://89.184.185.202:55555/api/userinfo', {
+        const response = await fetch('https://orbo-api.orbinuity.nl/api/userinfo', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
