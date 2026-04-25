0fc600dcfd75e431d55e2c48ff8a0c29a1498513
Flurop <flora@ratgers.nl>
2026-04-25 09:04:40 +0200

    Now using correct domain!

--- checkServer.js
+++ checkServer.js
@@ -1,12 +1,11 @@
 async function init() {
     try {
-        const response = await fetch('http://89.184.185.202:55555/api/isonline', {
+        const response = await fetch('https://orbo-api.orbinuity.nl/api/isonline', {
             method: 'GET'
         });
 
         if (!response.ok) {
             document.location.href = "/serverOffline";
-            return;
         }
     } catch (error) {
         document.location.href = "/serverOffline";
