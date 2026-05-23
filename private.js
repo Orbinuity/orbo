5c1f0c820c6e20e4a8d49003ef6ffea515addda4
Flurop <flora@ratgers.nl>
2026-05-23 17:42:20 +0200

    Added Privacy Policy check and chnaged server url

--- private.js
+++ private.js
@@ -1,11 +1,11 @@
-async function init() {
+async function initd() {
     const token = document.cookie
         .split("; ")
         .find(row => row.startsWith('token='))
         ?.split("=")[1];
 
     if (token) {
-        const response = await fetch('https://orbo-api.orbinuity.nl/api/userinfo', {
+        const response = await fetch('http://89.184.185.202:55555/api/userinfo', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -17,4 +17,4 @@ async function init() {
     } else document.location.href = "/";
 }
 
-init()
+initd()
