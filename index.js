15564771a7b7552d7795bf1814d5be47c792df72
Flurop <flora@ratgers.nl>
2026-05-24 20:52:59 +0200

    Added posts, users and better security

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-async function testToken(token, headerNav) {
-    if (token) {
-        const response = await fetch('https://orboapi.orbinuity.nl:55555/api/userinfo', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        });
+async function testToken() {
+    const token = document.cookie
+        .split("; ")
+        .find(row => row.startsWith('token='))
+        ?.split("=")[1];
 
-        if (response.ok) document.location.href = "/home";
-        else headerNav.innerHTML = "";
-    } else headerNav.innerHTML = "";
+    if (token) {
+        try {
+            const response = await fetch('https://orboapi.orbinuity.nl:55555/api/userinfo', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                document.location.href = "/home";
+            } else {
+                document.cookie = "token=; max-age=0; path=/";
+            }
+        } catch (err) {
+            console.error("Failed to fetch user info: "+err);
+        }
+    }
 }
 
-const observerc = new MutationObserver((mutations, obs) => {
-    const headerNav = document.getElementById("header-nav");
-    
-    if (headerNav) {
-        obs.disconnect(); 
-
-        const token = document.cookie
-            .split("; ")
-            .find(row => row.startsWith('token='))
-            ?.split("=")[1];
-
-        testToken(token, headerNav)
-    }
-});
-
-observerc.observe(document.documentElement, {
-    childList: true,
-    subtree: true
-});
+testToken();
