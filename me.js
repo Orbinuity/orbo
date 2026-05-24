d08357616449b73672d1af99063f9333c48970d5
Flurop <flora@ratgers.nl>
2026-05-24 20:53:00 +0200

    Added posts, users and better security

---
+++ me.js
@@ -0,0 +1,94 @@
+async function initd() {
+    const title = document.getElementById("title");
+    const username = document.getElementById("username");
+    const email = document.getElementById("email");
+    const name = document.getElementById("name");
+
+    const token = document.cookie
+        .split("; ")
+        .find(row => row.startsWith('token='))
+        ?.split("=")[1];
+
+    if (!token) document.location.href = "/";
+
+    try {
+        const response = await fetch('https://orboapi.orbinuity.nl:55555/api/userinfo', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            title.textContent = `Hello, ${data.user.name}!`;
+            username.placeholder = data.user.username;
+            email.placeholder = data.user.email;
+            name.placeholder = data.user.name;
+        } else {
+            alert(data.error);
+            return;
+        }
+    } catch (err) {
+        console.error("Failed to get user info: "+err);
+        return;
+    }
+}
+
+async function saveOptions(event) {
+    event.preventDefault(); 
+
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const name = document.getElementById("name").value.trim();
+    const opassword = document.getElementById("opassword").value.trim();
+
+    const formData = new FormData(document.getElementById('accountForm'));
+    const captchaToken = formData.get('g-recaptcha-response'); 
+
+    const token = document.cookie
+        .split("; ")
+        .find(row => row.startsWith('token='))
+        ?.split("=")[1];
+
+    if (!token) document.location.href = "/";
+
+    try {
+        const response = await fetch('https://orboapi.orbinuity.nl:55555/api/updateuser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                username: username,
+                email: email,
+                password: password,
+                name: name,
+                opassword: opassword,
+                captchaToken: captchaToken
+            })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            const date = new Date();
+            date.setTime(date.getTime() + (2 * 24 * 60 * 60 * 1000));
+
+            document.cookie = `token=${data.token}; expires=${date.toUTCString()}; path=/`;
+            document.location.href = "/me";
+        } else {
+            alert(data.error)
+            return;
+        }
+    } catch (error) {
+        console.error("Failed to update user info: "+error);
+        return;
+    }
+}
+
+initd();
